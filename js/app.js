bc59f7a940fb4682bb53ca39b6b65186f58073e2
Loan Tran <loantranvn810@gmail.com>
2018-07-09 15:18:48 -0500

    create a draft modal

--- js/app.js
+++ js/app.js
@@ -83,11 +83,8 @@ function numPairs() {
     console.log(counter); 
 }
 
-// var modal = document.querySelector("#modal");
-
 function allCardMatch() {
     // if all cards have matched, display a message with the final score 
-    // modal.style.display = "block"; 
     var timePlayed = timer.textContent; 
     console.log("Congratulations! You Won!"); 
     console.log("After " + timePlayed + " With " + counter + " Moves and " + starRating() + " Stars" );
@@ -244,3 +241,33 @@ restartBtn.addEventListener("click", function() {
 
 // WHEN BROWSER OPEN
 init(); 
+
+
+// MODAL
+var modal = document.getElementById("myModal"); // Get the modal
+var btn = document.getElementById("myBtn"); // Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+var playAgainBtn = document.getElementById("play-again"); 
+var modalTime = document.getElementById("time-played");
+var modalMoves = document.getElementById("num-moves");
+var modalStars = document.getElementById("star-rate"); 
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    modalTime.textContent = timer.textContent; 
+    modalMoves.textContent = counter; 
+    modalStars.textContent = starRating(); 
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
